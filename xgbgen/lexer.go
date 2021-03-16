2ff749be1d3b1883652b7a261342f1f0138d9fc1
Joshua Oakes <joshua.d.oakes@gmail.com>
2021-03-15 19:31:02 -0700

    working stash commit

--- xgbgen/lexer.go
+++ xgbgen/lexer.go
@@ -3,145 +3,86 @@ package xgbgen
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"io"
-	"text/scanner"
 )
 
 type tokenType int
 
+// token holds information about a scanned token
+type token struct {
+	tokenType
+	literal string
+}
+
+// tokenTypes
 const (
-	tokenEOF tokenType = iota
-	tokenError
-	tokenLeftComment
-	tokenComment
-	tokenRightComment
-	tokenText
+	tknError tokenType = iota
+	tknEOF
+	tknKeyword
+	tknIdentifier
+	tknConstant
+	tknLiteral
+	tknSymbol
+	tknText
 )
 
 var tokenMap = map[tokenType]string{
-	tokenEOF:          "end of file",
-	tokenError:        "error",
-	tokenLeftComment:  "/*",
-	tokenRightComment: "*/",
-	tokenText:         "text",
+	tknError:      "Error",
+	tknEOF:        "EOF",
+	tknKeyword:    "Keyword",
+	tknIdentifier: "Identifier",
+	tknConstant:   "Constant",
+	tknLiteral:    "Literal",
+	tknSymbol:     "Symbol",
 }
 
-type token struct {
-	tokenType
-	tokenLiteral string
-}
-
-func (t *token) String() string {
-	return fmt.Sprintf("{ tokenType: %s, tokenLiteral: %s", tokenMap[t.tokenType], t.tokenLiteral)
-}
-
-type lexer struct {
-	filename string
-	scanner  *bufio.Scanner
-	state    stateFn
-	tokens   chan token
-}
-
+// stateFn tracks state through lexical analysis
 type stateFn func(*lexer) stateFn
 
-func lex(filename string, input io.Reader) *lexer {
-	l := &lexer{
-		filename: filename,
-		scanner:  bufio.NewScanner(input),
-		state:    lexText,
-		tokens:   make(chan token, 2),
+// lexer ...
+type lexer struct {
+	name   string
+	reader *bufio.Reader
+	state  stateFn
+	tokens chan token
+}
+
+const (
+	// read delimeters
+	whitespace byte = ' '
+)
+
+func (l *lexer) read(delim byte) (string, error) {
+	return l.reader.ReadString(delim)
+}
+
+func (l *lexer) errorf(format string, args ...interface{}) stateFn {
+	l.tokens <- token{tknError, fmt.Sprintf(format, args...)}
+	return nil
+}
+
+// lex returns a new lexer, given an io.Reader
+func lex(name string, rdr io.Reader) (l *lexer) {
+	l = &lexer{
+		name:   name,
+		reader: bufio.NewReader(rdr),
+		state:  lexText,
+		tokens: make(chan token, 2),
 	}
 	return l
 }
 
-func (l *lexer) errorf(format string, args ...interface{}) stateFn {
-	l.tokens <- token{tokenError, fmt.Sprintf(format, args...)}
-	return nil
-}
-
-func (l *lexer) scan(split bufio.SplitFunc) rune {
-	l.scanner.Split(split)
-	return
-}
-
-func (l *lexer) peek() rune {
-	return l.scanner.Peek()
-}
-
-func (l *lexer) parse() token {
-	for {
-		select {
-		case tok := <-l.tokens:
-			return tok
-		default:
-			l.state = l.state(l)
-		}
-	}
-}
-
 const (
-	leftComment  = "/*"
-	rightComment = "*/"
+// keywords
 )
 
+// lexText ...
 func lexText(l *lexer) stateFn {
-	var buff bytes.Buffer
-	for tok := l.scan(); tok != scanner.EOF; tok = l.scan() {
-		switch tok {
-		case '/':
-			if l.peek() == '*' {
-				if buff.Len() > 0 {
-					l.tokens <- token{tokenText, buff.String()}
-				}
-				return lexLeftComment
-			}
-		default:
-			if _, err := buff.WriteString(l.scanner.TokenText()); err != nil {
-				return l.errorf("line: %d; %s", l.scanner.Line, err)
-			}
-		}
+	text, err := l.read(whitespace)
+	if err != nil {
+		l.errorf("Error reading while lexing text, %s", err)
 	}
-	if buff.Len() > 0 {
-		l.tokens <- token{tokenType: tokenText, tokenLiteral: buff.String()}
-	}
-	l.tokens <- token{tokenType: tokenEOF}
+
 	return nil
-}
-
-func lexLeftComment(l *lexer) stateFn {
-	if tok := l.scan(); tok == '*' {
-		l.tokens <- token{tokenLeftComment, leftComment}
-		return lexComment
-	}
-	return l.errorf("parsing error at line: %d", l.scanner.Line)
-}
-
-func lexComment(l *lexer) stateFn {
-	var buff bytes.Buffer
-	for tok := l.scan(); tok != scanner.EOF; tok = l.scan() {
-		switch tok {
-		case '*':
-			if l.peek() == '/' {
-				if buff.Len() > 0 {
-					l.tokens <- token{tokenComment, buff.String()}
-				}
-				return lexRightComment
-			}
-		default:
-			if _, err := buff.WriteString(l.scanner.TokenText()); err != nil {
-				return l.errorf("line: %d; %s", l.scanner.Line, err)
-			}
-		}
-	}
-	return l.errorf("non terminated comment")
-}
-
-func lexRightComment(l *lexer) stateFn {
-	if tok := l.scan(); tok == '/' {
-		l.tokens <- token{tokenRightComment, rightComment}
-		return lexText
-	}
-	return l.errorf("malformed comment, line: %d", l.scanner.Line)
 }

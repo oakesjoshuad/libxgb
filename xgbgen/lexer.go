3456ae06b217f4c56180eccef74c3e253eaba205
Joshua Oakes <joshua.d.oakes@gmail.com>
2021-03-19 11:58:18 -0700

    slowly refactoring xgbgen

--- xgbgen/lexer.go
+++ xgbgen/lexer.go
@@ -50,6 +50,7 @@ var tokenMap = map[tokenType]string{
 	tknInlineComment: "Inline Comment",
 }
 
+// String is a stringer method returning a string representation of a token
 func (tkn *token) String() string {
 	return fmt.Sprintf("{ %s : %s }", tokenMap[tkn.Type], tkn.Literal)
 }
@@ -62,7 +63,7 @@ type lexer struct {
 	name    string
 	scanner *bufio.Scanner
 	state   stateFn
-	text    string
+	linepos int
 	tokens  chan token
 }
 
@@ -75,17 +76,20 @@ func (l *lexer) checkScanErr() error {
 	return l.scanner.Err()
 }
 
+// emit checks that a token is not empty and puts it on the token channel
 func (l *lexer) emit(t tokenType, s string) {
 	if !isEmpty(s) {
 		l.tokens <- token{t, s}
 	}
 }
 
+// errorf ...
 func (l *lexer) errorf(format string, args ...interface{}) stateFn {
 	l.tokens <- token{tknError, fmt.Sprintf(format, args...)}
 	return nil
 }
 
+// nextToken returns a token from the token channel if it exists or moves to the next state
 func (l *lexer) nextToken() token {
 	for {
 		select {
@@ -103,9 +107,17 @@ func lex(name string, rdr io.Reader) *lexer {
 		name:    name,
 		scanner: bufio.NewScanner(rdr),
 		state:   lexText,
+		linepos: 0,
 		tokens:  make(chan token, 2),
 	}
 	l.scanner.Split(bufio.ScanLines)
+
+	key = map[string]stateFn{
+		cStruct:  lexCStruct,
+		cTypedef: lexCTypedef,
+		cDefine:  lexCDirective,
+	}
+
 	return l
 }
 
@@ -118,73 +130,100 @@ const (
 	leftComment   = "/*"
 	rightComment  = "*/"
 	inlineComment = "//"
-	directive     = "#"
 	semicolon     = ";"
 
-	// keywords
+	// keywords our lexer should be interested in
 	cStruct  = "struct"
 	cTypedef = "typedef"
+	cDefine  = "#define"
 )
+
+var key map[string]stateFn
 
 // lexText ...
 func lexText(l *lexer) stateFn {
-	for txt := ""; l.scan(); l.text = txt {
+	var txt string
+	for txt := ""; l.scan(); txt += l.scanner.Text() {
+		// comment bodies are non standardized, checking of our line is prefixed with
+		// an opening comment identifier, '/*' or '//'
 		if strings.HasPrefix(l.scanner.Text(), leftComment) {
-			l.text = strings.TrimPrefix(l.scanner.Text(), leftComment)
 			l.emit(tknText, txt)
 			return lexLeftComment
-		}
-		if strings.HasPrefix(l.scanner.Text(), directive) {
+		} else if strings.HasPrefix(l.scanner.Text(), inlineComment) {
 			l.emit(tknText, txt)
-			return lexDirective
+			return lexInlineComment
 		}
-		if strings.HasPrefix(l.scanner.Text(), cTypedef) {
-			l.emit(tknText, txt)
-			return lexCTypedef
+		// otherwise, I'm looking for a keyword
+		if words := strings.Fields(l.scanner.Text()); len(words) > 0 {
+			word := words[l.linepos]
+			if fn, ok := key[word]; ok {
+				l.emit(tknText, txt)
+				return fn
+			}
 		}
-		txt += l.scanner.Text()
 	}
+
 	// check for a scan error
 	if err := l.checkScanErr(); err != nil {
 		l.errorf("encountered an error while scanning text: %s", err)
 	}
 
 	// we have reached the end of the line
-	l.emit(tknEOF, l.text)
+	l.emit(tknEOF, txt)
 	return nil
 }
 
+// lexLeftCommenet  ...
 func lexLeftComment(l *lexer) stateFn {
+	if strings.HasSuffix(l.scanner.Text(), rightComment) {
+		l.emit(tknLeftComment, leftComment)
+		return lexInlineComment
+	}
 	l.emit(tknLeftComment, leftComment)
 	return lexInsideComment
 }
 
+// lexInsideComment ...
 func lexInsideComment(l *lexer) stateFn {
-	for txt := l.text; !strings.HasSuffix(txt, rightComment); l.text = txt {
-		if l.scan() {
-			txt = fmt.Sprintf("%s\n%s", txt, l.scanner.Text())
-		} else if l.scanner.Err() != nil {
-			l.errorf("encountered an error while scanning inside comment: %s", l.scanner.Err())
-		} else {
-			l.errorf("encountered end of file before comment termination")
+	var txt string
+	for txt = strings.TrimPrefix(l.scanner.Text(), leftComment); l.scan(); txt += l.scanner.Text() {
+		if strings.HasSuffix(l.scanner.Text(), rightComment) {
+			txt += strings.TrimSuffix(l.scanner.Text(), rightComment)
+			l.emit(tknCommentText, txt)
+			return lexRightComment
 		}
-
 	}
-	txt := strings.TrimSuffix(l.text, rightComment)
-	l.emit(tknCommentText, txt)
-	return lexRightComment
+	if err := l.checkScanErr(); err != nil {
+		return l.errorf("encountered an error scanning a comment: %s", err)
+	}
+	return l.errorf("encounted EOF while lexing inside comment: %s", txt)
 }
 
+// lexRightComment ...
 func lexRightComment(l *lexer) stateFn {
 	l.emit(tknRightComment, rightComment)
 	return lexText
 }
 
-func lexCTypedef(l *lexer) stateFn {
-	if 
+// lexInlineComment ...
+func lexInlineComment(l *lexer) stateFn {
+	txt := strings.Trim(l.scanner.Text(), leftComment+rightComment+inlineComment)
+	l.emit(tknInlineComment, txt)
+	return lexText
 }
 
-func lexStructure(l *lexer) stateFn {
+// lexCDirective ...
+func lexCDirective(l *lexer) stateFn {
+	return l.errorf("lexCDirective Not Implemented")
+}
+
+// lexCTypedef ...
+func lexCTypedef(l *lexer) stateFn {
+	return l.errorf("Error parsing Ctypedef")
+}
+
+// lexCStruct ...
+func lexCStruct(l *lexer) stateFn {
 	return l.errorf("Error lexing struct:")
 }
 
@@ -193,50 +232,15 @@ const (
 	define = "#define"
 )
 
-func lexDirective(l *lexer) stateFn {
-	if strings.HasPrefix(l.scanner.Text(), define) {
-		return lexConstant
-	}
-	return lexText
-}
-
 func lexConstant(l *lexer) stateFn {
-	if strings.HasPrefix(l.scanner.Text(), define) {
-		if len(strings.Fields(l.scanner.Text())) < 3 {
-			return lexText
-		}
-		l.text = strings.TrimPrefix(l.scanner.Text(), define)
-		l.emit(tknDirective, define)
-		return lexIdentifier
-	}
-	return lexText
+	return l.errorf("lexConstant Not Implemented")
 }
-
 func lexIdentifier(l *lexer) stateFn {
-	tkns := strings.Fields(l.text)
-	if !isWord(tkns[0]) {
-		l.errorf("error: invalid identifier: %s", tkns[0])
-	}
-	l.emit(tknIdentifier, tkns[0])
-	l.text = strings.Join(tkns[1:], " ")
-	return lexLiteral
+	return l.errorf("lexIdentifier Not Implemented")
 }
 
 func lexLiteral(l *lexer) stateFn {
-	tkns := strings.Fields(l.text)
-	if isNumber(tkns[0]) || isWord(tkns[0]) {
-		l.emit(tknLiteral, tkns[0])
-	}
-	l.text = strings.Join(tkns[1:], " ")
-	if strings.HasPrefix(l.text, leftComment) || strings.HasPrefix(l.text, inlineComment) {
-		return lexInlineComment
-	}
-	return lexText
-}
-
-func lexInlineComment(l *lexer) stateFn {
-	l.emit(tknInlineComment, l.text)
-	return lexText
+	return l.errorf("lexLiteral Not Implemented")
 }
 
 const (
